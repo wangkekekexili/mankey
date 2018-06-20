1eae6ca9b9d37bba262081058e1cce843699a03a
Ke Wang <wangkekekexili@gmail.com>
2018-06-19 20:39:09 -0400

    Make REPL more user-friendly.

--- repl/repl.go
+++ repl/repl.go
@@ -10,13 +10,21 @@ import (
 )
 
 func Do(r io.Reader, w io.Writer) {
+
+	fmt.Fprintf(w, ">> ")
 	scanner := bufio.NewScanner(r)
 	for scanner.Scan() {
-		p, err := parser.New(lexer.New(scanner.Text())).ParseProgram()
+		text := scanner.Text()
+		if text == "exit" {
+			return
+		}
+		p, err := parser.New(lexer.New(text)).ParseProgram()
 		if err != nil {
 			fmt.Fprintln(w, err)
+			fmt.Fprintf(w, ">> ")
 			continue
 		}
 		fmt.Fprintln(w, p)
+		fmt.Fprintf(w, ">> ")
 	}
 }
